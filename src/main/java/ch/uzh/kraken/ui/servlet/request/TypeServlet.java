ce99bfeb4c9709f5ffe590f9009f208d91f11a22
andrilareida <andrilareida@gmail.com>
2015-03-17 16:23:28 +0100

    forgot

---
+++ src/main/java/ch/uzh/kraken/ui/servlet/request/TypeServlet.java
@@ -0,0 +1,28 @@
+package ch.uzh.kraken.ui.servlet.request;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import ch.uzh.kraken.ui.database.api.DataFactory;
+import ch.uzh.kraken.ui.util.Util;
+
+@WebServlet("/request/types.json")
+public class TypeServlet extends HttpServlet{
+
+	
+	private static final long serialVersionUID = -7855451053390085840L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String type = request.getParameter("type");
+		if(type == null || type.isEmpty()){
+			Util.writeJsonResponse(response, DataFactory.getDataInterface().getTypes().toJSONString());
+		}else {
+			Util.writeJsonResponse(response, DataFactory.getDataInterface().getType(type).toJSONString());
+		}
+	}
+}
